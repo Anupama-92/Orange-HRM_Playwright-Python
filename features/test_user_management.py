6ddab28c4b9bdea9bd11e0becbc38bb3a2808f21
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2025-08-22 15:08:35 +0530

    Data driven testing using pytest(@pytest.mark.parametrize)

--- features/test_user_management.py
+++ features/test_user_management.py
@@ -1,20 +1,37 @@
+import json
+import pytest
 from pages.admin_page import AdminPage
 from pages.login_page import LoginPage
 from pages.user_management import UserManagementPage
 
 
-def test_user_management(page):
+def load_test_data():
+    with open("test_data/user_data.json", "r") as file:
+        return json.load(file)
+
+
+@pytest.mark.parametrize("user", load_test_data())
+def test_user_management(page, user):
     login_page = LoginPage(page)
     login_page.navigate()
     login_page.enter_username()
     login_page.enter_password()
     login_page.click_login()
+
     admin_page = AdminPage(page)
     admin_page.navigate_to_admin_page()
+
     user_management_page = UserManagementPage(page)
     user_management_page.navigate_to_user_management_page()
     user_management_page.select_user_menu()
     user_management_page.click_add_user()
-    user_management_page.click_user_role_dropdown()
-    user_management_page.select_user_role()
 
+    # Dynamic data from JSON
+    user_management_page.click_user_role_dropdown()
+    user_management_page.select_user_role()  # You can also make role dynamic if needed
+    user_management_page.select_status()
+    user_management_page.enter_employee_name(user["employee_name"])
+    user_management_page.username.fill(user["username"])
+    user_management_page.password.fill(user["password"])
+    user_management_page.confirm_password.fill(user["confirm_password"])
+    user_management_page.click_save_button()
--- pages/user_management.py
+++ pages/user_management.py
@@ -8,7 +8,15 @@ class UserManagementPage:
         self.users = page.get_by_role("menuitem", name="Users")
         self.add_user = page.get_by_role("button", name="Add")
         self.user_role_dropdown = self.page.locator("//label[text()='User Role']/../following-sibling::div//div[contains(@class,'oxd-select-text')]")
+        self.status_dropdown_locator = self.page.locator("//label[text()='Status']/../following-sibling::div//div[contains(@class,'oxd-select-text-input')]")
         self.user_role = page.get_by_role("option", name="Admin")
+        self.status_option = page.get_by_role("option", name="Enabled")
+        self.employee_name = page.get_by_role("textbox", name="Type for hints...")
+        self.username = page.locator("//label[text()='Username']/../following-sibling::div//input")
+        self.password = page.locator("//label[text()='Password']/../following-sibling::div//input")
+        self.confirm_password = page.locator("//label[text()='Confirm Password']/../following-sibling::div//input")
+        self.save_button = page.get_by_role("button", name="Save")
+
 
     def navigate_to_user_management_page(self):
         self.page.pause()
@@ -25,3 +33,30 @@ class UserManagementPage:
 
     def select_user_role(self):
         self.user_role.click()
+
+    def select_status(self):
+        self.status_dropdown_locator.click()
+        self.status_option.click()
+
+    def enter_employee_name(self, employee_name):
+        # Fill the text box
+        self.employee_name.fill(employee_name)
+
+        # Wait for the suggestion list to appear and select the matching option
+        suggestion = self.page.locator(f"//div[@role='listbox']//span[text()='{employee_name}']")
+        suggestion.wait_for(state="visible", timeout=5000)
+        suggestion.click()
+
+    def enter_username(self, username):
+        self.username.fill(username)
+
+    def enter_password(self, password):
+        self.password.fill(password)
+
+    def enter_confirm_password(self, confirm_password):
+        self.confirm_password.fill(confirm_password)
+
+    def click_save_button(self):
+        self.save_button.click()
+
+
