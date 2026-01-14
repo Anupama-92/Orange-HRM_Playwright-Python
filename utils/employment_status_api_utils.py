7b4d02906d214a3b2cbd1f955bf31b48c1a0bcd7
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2026-01-14 18:22:55 +0530

    Updated the code

--- utils/employment_status_api_utils.py
+++ utils/employment_status_api_utils.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 from config.config import Configs
 
@@ -9,11 +10,20 @@ class EmploymentStatusAPI:
     def get_all_employment_statuses(cls):
         """Fetch all employment statuses"""
         response = cls.session.get(Configs.EMPLOYMENT_STATUS_POSTURL, headers=Configs.HEADERS)
-        response.raise_for_status()
+        if response.status_code != 200:
+            allure.attach(
+                response.text,
+                f"GET Employment Statuses Failed ({response.status_code})",
+                allure.attachment_type.TEXT
+            )
+            raise AssertionError(
+                f"GET employment statuses failed with {response.status_code}"
+            )
+        # response.raise_for_status()
         return [status["name"] for status in response.json().get("data", [])]
 
     @classmethod
-    def add_employment_status(cls, name="TestAuto77"):
+    def add_employment_status(cls, name="TestAuto78"):
         """Add a new employment status"""
         payload = {"name": name}
 
@@ -22,5 +32,14 @@ class EmploymentStatusAPI:
             headers=Configs.HEADERS,
             json=payload
         )
-        response.raise_for_status()
+        if response.status_code not in (200, 201):
+            allure.attach(
+                response.text,
+                f"POST Employment Status Failed ({response.status_code})",
+                allure.attachment_type.TEXT
+            )
+            raise AssertionError(
+                f"POST employment status failed with {response.status_code}: {response.text}"
+            )
+        # response.raise_for_status()
         return response.json()["data"]["name"]
