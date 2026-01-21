31fbda05a2b00756ef9bfb2bd68b62fcd3a66079
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2026-01-21 16:06:24 +0530

    Latest code

--- utils/employment_status_api_utils.py
+++ utils/employment_status_api_utils.py
@@ -23,7 +23,7 @@ class EmploymentStatusAPI:
         return [status["name"] for status in response.json().get("data", [])]
 
     @classmethod
-    def add_employment_status(cls, name="TestAuto79"):
+    def add_employment_status(cls, name="TestAuto81"):
         """Add a new employment status"""
         payload = {"name": name}
 
